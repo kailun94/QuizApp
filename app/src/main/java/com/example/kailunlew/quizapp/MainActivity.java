dcb60aeed0986c908fff3de2d23999abb2adc633
Kailun Lew <kailunlew@Kailuns-MacBook-Pro.local>
2017-12-06 00:07:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kailunlew/quizapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.kailunlew.quizapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStartButton = (Button)findViewById(R.id.startButton);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,QuizActivity.class));
+
+            }
+        });
+
+    }
+}
+
